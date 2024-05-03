4777e2a94530ce5bb4543e6088c64ce9c9802d3e
kevin <kevinlac@tuta.io>
2024-05-03 13:54:03 +0930

    chore: alternate movement keys

--- src/Player.cpp
+++ src/Player.cpp
@@ -7,16 +7,16 @@ Player::Player(Game* game) : GameEntity(game) {}
 void Player::Update(float dt) {
 	// Move around using arrow keys scaled by speed and dt
 	raylib::Vector2 movement = raylib::Vector2(0, 0);
-	if (IsKeyDown(KEY_LEFT)) {
+	if (IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A)) {
 		movement.x--;
 	}
-	if (IsKeyDown(KEY_UP)) {
+	if (IsKeyDown(KEY_UP) || IsKeyDown(KEY_W)) {
 		movement.y--;
 	}
-	if (IsKeyDown(KEY_DOWN)) {
+	if (IsKeyDown(KEY_DOWN) || IsKeyDown(KEY_S)) {
 		movement.y++;
 	}
-	if (IsKeyDown(KEY_RIGHT)) {
+	if (IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D)) {
 		movement.x++;
 	}
 	raylib::Vector2 pos = get_pos();
