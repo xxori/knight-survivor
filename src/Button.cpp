b21d189032da382d0f7ca4ca51a25cbfd232ff7b
xxori <ori@kek.church>
2024-05-12 22:18:38 +0930

    fix/feat: Fix click detection + add hover color + run formatter

--- src/Button.cpp
+++ src/Button.cpp
@@ -3,25 +3,31 @@
 Button::Button(Game* game) : Button(game, raylib::Vector2(0, 0), WHITE, "", 0, 0, WHITE) {}
 
 Button::Button(Game* game, raylib::Vector2 pos, raylib::Color color, std::string text, int margin, int fontSize, raylib::Color fontColor) : GameObject(game, pos), color(color), text(text), margin(margin), fontSize(fontSize), fontColor(fontColor) {}
-    
+
 void Button::update(float dt) {
-    if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
-        raylib::Vector2 mousePos = GetMousePosition();
-        int width = MeasureText(text.c_str(), fontSize);
-        if (mousePos.x >= this->getPos().x && mousePos.y >= this->getPos().y
-        && mousePos.x <= this->getPos().x + width + 2*margin 
-        && mousePos.y <= this->getPos().y + fontSize * 2*margin) { // if mouseclick inside button area
-            // do something
-            text = "Clicked";
-        }
-    }
+	raylib::Vector2 mousePos = GetMousePosition();
+	int width = MeasureText(text.c_str(), fontSize);
+
+	if (mousePos.x >= this->getPos().x && mousePos.y >= this->getPos().y && mousePos.x <= this->getPos().x + width + 2 * margin && mousePos.y <= this->getPos().y + fontSize + 2 * margin) { // if mouseclick inside button area
+																																																																																													 // do something
+		if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+			// If clicked
+			this->setText("Clicked");
+		} else {
+			// If hovered
+			this->setColor(raylib::Color::FromHSV(0, 68, 100));
+		}
+	} else {
+		// Reset color
+		this->setColor(RED);
+	}
 }
 
 void Button::draw() {
-    int width = MeasureText(text.c_str(), fontSize);
-    DrawRectangle(this->getPos().x, this->getPos().y, width + 2*margin, fontSize + 2*margin, color);
-    DrawText(text, this->getPos().x + margin, this->getPos().y + margin, fontSize, fontColor);
-    // get size of text
+	int width = MeasureText(text.c_str(), fontSize);
+	DrawRectangle(this->getPos().x, this->getPos().y, width + 2 * margin, fontSize + 2 * margin, color);
+	DrawText(text, this->getPos().x + margin, this->getPos().y + margin, fontSize, fontColor);
+	// get size of text
 }
 
 raylib::Color Button::getColor() { return color; }
--- src/Button.hpp
+++ src/Button.hpp
@@ -4,26 +4,27 @@
 
 class Button : public GameObject {
 private:
-    std::string text;
-    int margin;
-    int fontSize;
-    raylib::Color fontColor;
-    raylib::Color color;
-public:
-    Button(Game* game);
-    Button(Game* game, raylib::Vector2 pos, raylib::Color color, std::string text, int margin, int fontSize, raylib::Color fontColor);
-    
-    void update(float dt);
-	void draw();
+	std::string text;
+	int margin;
+	int fontSize;
+	raylib::Color fontColor;
+	raylib::Color color;
 
-    raylib::Color getColor();
-    void setColor(raylib::Color color);
-    void setText(std::string text);
-    std::string getText();
-    void setMargin(int margin);
-    int getMargin();
-    void setFontSize(int fontSize);
-    int getFontSize();
-    void setFontColor(raylib::Color color);
-    raylib::Color getFontColor();
+public:
+	Button(Game* game);
+	Button(Game* game, raylib::Vector2 pos, raylib::Color color, std::string text, int margin, int fontSize, raylib::Color fontColor);
+
+	void update(float dt) override;
+	void draw() override;
+
+	raylib::Color getColor();
+	void setColor(raylib::Color color);
+	void setText(std::string text);
+	std::string getText();
+	void setMargin(int margin);
+	int getMargin();
+	void setFontSize(int fontSize);
+	int getFontSize();
+	void setFontColor(raylib::Color color);
+	raylib::Color getFontColor();
 };
