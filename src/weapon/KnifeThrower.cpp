20a335bca48152a78a0e9a14fb63d6aa1bf5f673
xxori <ori@kek.church>
2024-05-24 15:10:50 +0930

    feat: update knife damage to 3

--- src/weapon/KnifeThrower.cpp
+++ src/weapon/KnifeThrower.cpp
@@ -9,6 +9,6 @@ void KnifeThrower::update(float dt) {
 	setTimeToFire(getTimeToFire() - dt);
 	if (getTimeToFire() <= 0) {
 		setTimeToFire(getFireSpeed());
-		getGame()->addObject(new Knife(getGame(), 150 + getGame()->getPlayer()->getSpeed(), 2, -getGame()->getPlayer()->getDirection()));
+		getGame()->addObject(new Knife(getGame(), 150 + getGame()->getPlayer()->getSpeed(), 3, -getGame()->getPlayer()->getDirection()));
 	}
 }
